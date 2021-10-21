bc709a32f85d114256c072083b2fb34cd17fa6d9
tiffanyfy <tiffanyyiu3@gmail.com>
2021-10-20 23:32:00 -0700

    Added accordion functionality to index.js

---
+++ dist/scripts/index.js
@@ -0,0 +1,15 @@
+/* Animated Accordion (on individual project pages) */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
